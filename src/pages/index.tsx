c3bfd0e19f9c5c1cb0143497a067675fc99acfab
shakil.ot <shakil@orangetoolz.com>
2022-06-12 16:25:30 +0600

    init project commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+/**
+ * this is the home page
+ * @return {JSX.Element} : jsx code for home page
+ */
+export default function Home() {
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  )
+}
