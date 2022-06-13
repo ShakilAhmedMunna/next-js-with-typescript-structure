5a98ca74f397bd4746eee073f4c78c76d3c8a14f
shakil.ot <shakil@orangetoolz.com>
2022-06-13 14:45:51 +0600

    basic structure done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,29 +3,53 @@ import Head from "next/head";
 import Addition from "../component/view/calculator/Addition";
 import PostWrapper from "../component/view/blog/PostWrapper";
 
+interface HomeInterface {
+  pageTitle: string,
+  descriptionContent: string,
+  postTitle: string
+}
 
 /**
  * this is the home page
  * @return {JSX.Element} : jsx code for home page
  */
-const Home = (): JSX.Element => {
+const Home = ({ pageTitle, descriptionContent, postTitle }: HomeInterface): JSX.Element => {
   return (
     <>
       <Head>
-        <title>My page title</title>
+        <title>{pageTitle}</title>
         <meta property="og:title" content="My page title" key="title" />
-        <meta name="description" content="Meta description content goes here." />
+        <meta name="description" content={descriptionContent} />
         <link rel="shortcut icon" href="/favicon.png" />
       </Head>
 
-
-      <h1>Hello </h1>
-      <PostWrapper title={"Addition 2 number"}>
+      <PostWrapper title={postTitle}>
         <Addition firstNumber={12} secondNumber={8} />
       </PostWrapper>
 
     </>
   );
 };
+
+/**
+ *
+ * @param context
+ */
+
+/**
+ *
+ * @param {any} context
+ */
+export async function getServerSideProps(context: any) {
+
+  return {
+    props: {
+      pageTitle: "This is a demo",
+      descriptionContent: "Meta description content goes here.",
+      postTitle: "Addition 2 number",
+    },
+  };
+
+}
 
 export default Home;
